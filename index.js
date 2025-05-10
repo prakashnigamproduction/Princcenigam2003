c6bca4fbd90e71140f2a20f78f80d94c789e750e
prakashnigamproduction <pubgkings2003@gmail.com>
2025-05-10 15:27:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const ytdl = require('ytdl-core');
+const ytdl = require('@distube/ytdl-core');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
